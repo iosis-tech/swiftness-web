1bf6bad64a4a339a3905ed0007275813b17f9391
Bartosz Nowak <bartekn337@gmail.com>
2024-07-20 20:18:55 +0200

    address bars color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Set the theme color for Android Chrome */}
+        <meta name="theme-color" content="#000" />
+        {/* Set the theme color for iOS Safari */}
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#000" />
+        {/* Set the theme color for Windows Phone */}
+        <meta name="msapplication-navbutton-color" content="#000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
